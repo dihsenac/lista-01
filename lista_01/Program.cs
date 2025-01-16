beefd58526d0ee253c2f21f60bfe97b47ad7660b
ERIBERTO DIEGO PEREIRA <eriberto.dpereira@senacsp.edu.br>
2025-01-16 14:57:21 -0300

    lista_01

---
+++ lista_01/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Verificação de paridade
+ 
+
+Objetivo: Pratique o uso do operador % (módulo) e estruturas condicionais simples. 
+
+Descrição: Crie um programa que solicite que o usuário digite um número inteiro. O programa deve verificar se o número é par ou ímpar. Use a condição if para determinar o resultado e exibir uma mensagem apropriada. 
+
+Exemplo de saída: "O número é par." ou "O número é ímpar".
+*/
+Console.WriteLine("Digite um numero inteiro ");
+
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0) ;
+
+
+Console.WriteLine("o numero é par");
+
+else ("o numero é impar");
+
